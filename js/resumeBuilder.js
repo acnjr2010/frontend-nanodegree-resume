2711e10d2f71eaa7a553fcfc3d0183cd307cf1d1
Junior <nog.junior84@gmail.com>
2016-10-03 20:40:55 -0300

    append and replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Antonio Carlos Nogueira Junior";
+var role = "Web Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
